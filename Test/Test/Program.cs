fedfdeeae791a172b84b50b616e3272bc2d77fb1
Johnathan <johnathanleech09@gmail.com>
2018-08-30 15:04:24 -0400

    Trying test again

---
+++ Test/Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int asciiValue = Console.Read();     // can be used to find the ascii value
+            Console.WriteLine(asciiValue);
+        }
+    }
+}
