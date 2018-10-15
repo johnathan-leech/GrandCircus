0d9212e72eef4cc7b7355e8104f146867134fd77
Johnathan Leech <johnathanleech09@gmail.com>
2018-10-15 14:31:55 -0400

    doesn't work. length throws error

--- Capstone1/Capstone1.cs
+++ Capstone1/Capstone1.cs
@@ -4,49 +4,67 @@ namespace Capstone1
 {
     class Capstone1
     {
-        static string finalString;
+        static string finalString = "";
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Pig Latin Translator");
 
+            TranslateToPigLatin(GetsUserInput());
+
+            Console.WriteLine(finalString);
+        }
+
+        static string GetsUserInput()
+        {
             Console.Write("Enter a line to be translated: ");
-            string sentence = Console.ReadLine().ToLower();
+            string sentence;
+            return sentence = Console.ReadLine().ToLower();
+        }
 
 
+        static void TranslateToPigLatin(string sentence)
+        {
             string vowels = "aeiou";
+            string[] words = sentence.Split(' ');
 
-            foreach (string words in sentence.Split())
+            foreach (string word in words)
             {
-                string beginningLetter = sentence.Substring(0, 1);
-                string stemOfWord = sentence.Substring(1, words.Length - 1);
 
-                if (vowels.Contains(beginningLetter))
+                string firstLetter = word.Substring(0, 1);
+                string secondLetter = word.Substring(1, 1);
+                string thirdLetter = word.Substring(2, 1);
+
+                string wordStem = word.Substring(1, word.Length - 1);
+                string wordStem2 = word.Substring(2, word.Length - 2);
+                string wordStem3 = word.Substring(3, word.Length - 3);
+
+                int indexTestVowel = vowels.IndexOf(firstLetter);
+                int indexTestVowel2 = vowels.IndexOf(secondLetter);
+                int indexTestVowel3 = vowels.IndexOf(thirdLetter);
+
+                if (indexTestVowel == -1)
                 {
-                    finalString = words + "way ";
+                    if (indexTestVowel2 == -1)
+                    {
+                        if (indexTestVowel3 == -1)
+                        {
+                            finalString += wordStem3 + firstLetter + secondLetter + thirdLetter + "ay ";
+                        }
+                        finalString += wordStem2 + firstLetter + secondLetter + "ay ";
+                    }
+                    else
+                    {
+                        finalString += wordStem + firstLetter + "ay ";
+                    }
                 }
-                if (!(vowels.Contains(beginningLetter)))
+                else if (indexTestVowel == 0)
                 {
-                    finalString = stemOfWord + beginningLetter + "ay ";
+                    finalString += firstLetter + wordStem + "way ";
                 }
+
             }
 
-
-
-            //for (int i = 0; i < words.Length; i++)
-            //{
-            //    string beginningLetter = words[i].Substring(0,1);
-            //    string stemOfWord = words[i].Substring(1, words.Length - 2);
-
-            //    if (vowels.Contains(beginningLetter))
-            //    {
-            //        finalString = words[i] + "way ";
-            //    }
-            //    if (!(vowels.Contains(beginningLetter)))
-            //    {
-            //        finalString = stemOfWord + beginningLetter + "ay ";
-            //    }
-            //}
-            Console.WriteLine(finalString);
         }
     }
 }
