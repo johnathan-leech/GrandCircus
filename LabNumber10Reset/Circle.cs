38e69a54112e17c28bc87d6874a1cc967e9348dd
Johnathan Leech <johnathanleech09@gmail.com>
2018-10-24 09:09:52 -0400

    Submitting after having to make a new project due to not commiting to gitHub

---
+++ LabNumber10Reset/Circle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace LabNumber10
+{
+    class Circle
+    {
+        public const double PI = Math.PI;
+        private double radius;
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+        public Circle(double rad)
+        {
+            radius = rad;
+        }
+        public string FormatNumber(double x)
+        {
+            return $"{x,0:N2}";
+        }
+        public double CaluculateCircumference()
+        {
+            double circumference = PI * (radius * 2);
+            return circumference;
+        }
+        public string CaluculateFormattedCircumference()
+        {
+            return FormatNumber(CaluculateCircumference());
+        }
+        public double CalculateArea()
+        {
+            double area = PI * (radius * radius);
+            return area;
+        }
+        public string CalculatedFormatedArea()
+        {
+            return FormatNumber(CalculateArea());
+        }
+    }
+}
