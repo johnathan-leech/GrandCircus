48fd321ce598672aad258be278ce80d380e2fbc7
Johnathan Leech <johnathanleech09@gmail.com>
2018-11-08 07:57:23 -0500

    commiting Lab 21

---
+++ CoffeeShopApplication/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using CoffeeShopApplication.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace CoffeeShopApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult UserRegistration()
+        {
+            return View();
+        }
+
+        IEnumerable<char> CharsToTitleCase(string s)
+        {
+            bool newWord = true;
+            foreach (char c in s)
+            {
+                if (newWord) { yield return char.ToUpper(c); newWord = false; }
+                else yield return char.ToLower(c);
+                if (c == ' ') newWord = true;
+            }
+        }
+
+        public ActionResult AddUser(UserInfo newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                string firstName = new string(CharsToTitleCase(newUser.FirstName).ToArray());
+
+                ViewBag.ConfMessage1 = "Welcome " + firstName + " " + newUser.LastName;
+                ViewBag.ConfMessage2 = "Your email address: " + newUser.Email;
+                ViewBag.ConfMessage3 = "Your phone number: " + newUser.YoDigits;
+
+                return View("Confirm");
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+    }
+}
---
+++ CoffeeShopApplication/Models/UserInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShopApplication.Models
+{
+    public class UserInfo
+    {
+        [Required] // attributes 
+        [RegularExpression(@"^[a-zA-Z]{2,}$")]
+        public string FirstName { set; get; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]{2,}$")]
+        public string LastName { set; get; }
+
+        [Required]
+        [RegularExpression(@"[A-z0-9]{4,}@[A-z0-9]{2,}\.[A-z0-9]{1,}$")]
+        public string Email { set; get; }
+
+        [Required]
+        [RegularExpression(@"(\+?1?[0-9]{10})|([0-9]{3}[- ][0-9]{3}[- ][0-9]{4})|(\(\d{3}\)\d{3}[- ]\d{4})")]
+        public string YoDigits { set; get; }
+
+        public UserInfo()
+        {
+            FirstName = ""; LastName = ""; Email = ""; YoDigits = "";
+        }
+
+        public UserInfo(string firstN, string lastN, string email, string digits)
+        {
+            FirstName = firstN;
+            LastName = lastN;
+            Email = email;
+            YoDigits = digits;
+        }
+    }
+}
